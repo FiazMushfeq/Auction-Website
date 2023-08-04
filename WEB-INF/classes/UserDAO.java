b174a16a07eca2819c2d5785e5eedad1af7d7759
Shantanu Jain <sj919@scarletmail.rutgers.edu>
2023-08-04 00:46:31 -0400

    Made changes to website homepage and added a logout button

--- WEB-INF/classes/UserDAO.java
+++ WEB-INF/classes/UserDAO.java
@@ -3,27 +3,31 @@ import java.sql.*;
 public class UserDAO {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/auction-website?useSSL=false&serverTimezone=UTC";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "Your Password";
+    private static final String DB_PASSWORD = "Enter your password";
 
-    public User getUser(String email, String password) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+public User getUser(String email, String password) {
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user WHERE email = ? AND password = ?");
-            pstmt.setString(1, email);
-            pstmt.setString(2, password);
-            ResultSet rs = pstmt.executeQuery();
+        PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user WHERE email = ?");
+        pstmt.setString(1, email);
+        ResultSet rs = pstmt.executeQuery();
 
-            if (rs.next()) {
+        if (rs.next()) {
+            if (rs.getString("password").equals(password)) {
                 return new User(rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("password"));
+            } else {
+                return new User(null, null, null, null);  // wrong password
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-        return null;
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+
+    return null;  // email not found
+}
+
 
     public void saveUser(User user) {
         try {
--- login.jsp
+++ login.jsp
@@ -5,10 +5,16 @@
 </head>
 <body>
     <form action="LoginServlet" method="post">
-        Email: <input type="text" name="email" /><br/>
+        Email: <input type="email" name="email" /><br/>
         Password: <input type="password" name="password" /><br/>
         <input type="submit" value="Login" />
     </form>
     <button onclick="window.location.href='register.jsp'">Sign Up</button>
+
+    <% String errorMessage = (String) request.getAttribute("errorMessage"); %>
+    <% if (errorMessage != null) { %>
+        <p><%= errorMessage %></p>
+    <% } %>
 </body>
+
 </html>
