7985561db6ae70a66367de4bb252fd97ee5048ff
Shantanu Jain <sj919@scarletmail.rutgers.edu>
2023-08-03 23:34:57 -0400

    login page and registeration page is now working!

--- WEB-INF/classes/UserDAO.java
+++ WEB-INF/classes/UserDAO.java
@@ -1,16 +1,16 @@
 import java.sql.*;
 
 public class UserDAO {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/auction-website";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/auction-website?useSSL=false&serverTimezone=UTC";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "Enter your Password";
+    private static final String DB_PASSWORD = "Your Password";
 
     public User getUser(String email, String password) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE email = ? AND password = ?");
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM user WHERE email = ? AND password = ?");
             pstmt.setString(1, email);
             pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery();
@@ -27,10 +27,10 @@ public class UserDAO {
 
     public void saveUser(User user) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO user (firstName, lastName, email, password) VALUES (?, ?, ?, ?)");
             pstmt.setString(1, user.getFirstName());
             pstmt.setString(2, user.getLastName());
             pstmt.setString(3, user.getEmail());
